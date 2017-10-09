9dbd02b55cc25d7719948debf42cbe40b82ecbe1
karatekid <mchristen96@gmail.com>
2017-10-08 21:31:58 -0700

    More concise install

--- install.sh
+++ install.sh
@@ -1,15 +1,30 @@
 #!/bin/sh
 
+echo "Linking configuration files into $HOME";
+
 for f in $(find files/ -mindepth 1 -maxdepth 1); do
-  echo "Linking ${f} into home directory";
-  # Use -sf if desire to force override
-  ln -s -t ~/ $(realpath $f);
+  f_base = basename $f;
+  if [ "$(readlink $HOME/$f_base)" -ef "$f" ]; then
+      echo "${f} already linked"
+  else
+      echo "Linking ${f} into home directory";
+      # Use -sf if desire to force override
+      ln -s -t $HOME/ $(realpath $f);
+  fi
 done
 
+
+echo "Installing tools";
 # Install tools
 # Oh-my-zsh
-git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+if [ ! -d $HOME/.oh-my-zsh ]; then
+    git clone git://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh;
+fi
 # Configure zsh as default shell
-chsh -s $(which zsh)
-# Reload zsh config
-source ~/.zshrc
+if [ -n "`$SHELL -c 'echo $ZSH_VERSION'`" ]; then
+    # Assume in zsh
+    echo "zsh already configured";
+else
+    echo "Switching default shell to zsh, enter password:";
+    chsh -s $(which zsh);
+fi
