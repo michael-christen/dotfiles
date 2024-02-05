e85bab07a94a54d3e38d3805679626a4f70ba0f5
michael-christen <mchristen96@gmail.com>
2024-02-04 17:02:47 -0800

    Clean

--- setup.sh
+++ setup.sh
@@ -1,35 +1,11 @@
 #!/usr/bin/env bash
 
-# Manage these various files:
-# $PWD/profile -> .profile
-# $PWD/bash/bashrc  -> .bashrc
-# $PWD/bash/bash_profile.bash  -> .bash_profile
-# $PWD/ssh/authorized_keys -> .ssh/authorized_keys
-# $PWD/ssh/config -> .ssh/config
-# $PWD/inputrc -> .inputrc
-# Make directories
-# ${XDG_DATA_HOME:~/.local/share}/bash
-# ~/.ssh
-# Tools supported / tracked
-# - git
-# - bash
-# - nvim
-
-# Do I want these? set -o errexit
-# set -o pipefail
-
-# XXX: Remove need for submodules, plugins, etc ...
-# echo "Updating any git submodules"
-# git submodule update --init --recursive
-
 SETUP_FILE_PATH=$(realpath $0)
 cd "$(dirname "$SETUP_FILE_PATH")"
 NEW_XDG_CONFIG_HOME=$(dirname "$SETUP_FILE_PATH")/xdg_config_dir
 
-# XXX: Consider doing this
-# Update this repository
-# NOTE: `command -v "cmd" 2>&1 > /dev/null` checks for existence of `cmd`
-# command -v git 2>&1 > /dev/null && (git pull | tail -n +2)  # XXX if there's updates, re-exec
+# TODO: Update this repository
+# command -v git 2>&1 > /dev/null && (git pull | tail -n +2)  # TODO if there's updates, re-exec
 
 # Set XDG_CONFIG_HOME to the directory xdg_config_dir
 sed "s,export XDG_CONFIG_HOME=.*,export XDG_CONFIG_HOME=\"$NEW_XDG_CONFIG_HOME\"," template_config/profile > ~/.profile
@@ -41,59 +17,59 @@ command -v bash 2>&1 > /dev/null && ln -sf "$PWD/linked_config/bash/bash_profile
 command -v bash 2>&1 > /dev/null && ln -sf "$PWD/linked_config/zsh/zshenv" ~/.zshenv
 # Load any new bashrc settings.
 source ~/.bashrc
-# XXX: What to do about aliases?
+# TODO: What to do about aliases?
 
-
-# # Generate SSH Key
-# # From
-# # https://help.github.com/articles/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent/
-# if [ ! -f $HOME/.ssh/id_rsa.pub ]; then
-#     echo "No ssh key exists, generating it now in $HOME/.ssh/id_rsa.pub!";
-#     read -p "Enter github email address: " email_address
-#     ssh-keygen -t rsa -b 4096 -C $email_address;
-#     eval "$(ssh-agent -s)";
-#     ssh-add $HOME/.ssh/id_rsa;
-# fi
-# XXX: Remove .pub?
 command -v ssh 2>&1 > /dev/null && mkdir -p ~/.ssh \
-    && (for FILE in $(find "$PWD/linked_config/ssh" -name authorized_keys -o -name '*.pub' -o -name 'config'); do ln -sf "$FILE" ~/.ssh/; done)
+    && (for FILE in $(find "$PWD/linked_config/ssh" -name authorized_keys -o -name 'config'); do ln -sf "$FILE" ~/.ssh/; done)
 
 [ ! -f ~/.inputrc ] && ln -sf "$PWD/linked_config/inputrc" ~/.inputrc
 
 # Tmux
-# Download tpm if not present
+# Download tpm if not present and install plugins
 if [ ! -d $XDG_CONFIG_HOME/tmux/plugins/tpm ]; then
     git clone https://github.com/tmux-plugins/tpm $XDG_CONFIG_HOME/tmux/plugins/tpm
     $XDG_CONFIG_HOME/tmux/plugins/tpm/bin/install_plugins
 fi
-
 # Download Vundle if not present
 if [ ! -d $XDG_CONFIG_HOME/vim/bundle/Vundle.vim ]; then
 	  git clone https://github.com/VundleVim/Vundle.vim.git $XDG_CONFIG_HOME/vim/bundle/Vundle.vim
 fi
-
-# XXX: Install neovim / vim updates
-# HASNVIM=$(command -v nvim 2> /dev/null)
-# [ "$HASNVIM" ] && [ "$XDG_CONFIG_HOME" ] \
-#     && command -v git 2>&1 > /dev/null \
-#     && nvim --headless -c 'autocmd User MasonUpdateAll quitall' -c 'quitall' > /dev/null
-# # XXX I want to view the change logs, maybe have them be emailed?
-# #     && nvim --headless -c 'autocmd User Lazy update quitall' -c 'quitall' > /dev/null \
+# Download oh-my-zsh if not present
+if [ ! -d $XDG_CONFIG_HOME/zsh/.oh-my-zsh ]; then
+    git clone git@github.com:ohmyzsh/ohmyzsh.git $XDG_CONFIG_HOME/zsh/.oh-my-zsh;
+fi
 
 # Install vim plugins
-# NOTE: UI will pop up
-# XXX: Disabled because noisy
-# echo "Installing vim plugins";
-# vim -c 'PluginInstall' -c 'qa!';
-# echo "Finished installing vim plugins";
+vim -c 'PluginInstall' -c 'qa!' --headless 2> /dev/null;
 
+# Oh-my-zsh
+# Configure zsh as default shell
+if [ -n "`$SHELL -c 'echo $ZSH_VERSION'`" ]; then
+    # Assume in zsh
+    echo "" > /dev/null;
+else
+    echo "Switching default shell to zsh, enter password:";
+    chsh -s $(which zsh);
+    echo "You may have to restart your terminal for changes to take effect";
+fi
+
+# Generate SSH Key
+# From
+# https://help.github.com/articles/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent/
+if [ ! -f $HOME/.ssh/id_rsa.pub ]; then
+    echo "No ssh key exists, generating it now in $HOME/.ssh/id_rsa.pub!";
+    read -p "Enter github email address: " email_address
+    ssh-keygen -t rsa -b 4096 -C $email_address;
+    eval "$(ssh-agent -s)";
+    ssh-add $HOME/.ssh/id_rsa;
+fi
+
+# TODO: Consider auto-pulling this and adding other crontab jobs
 # Setup cron
 # echo "Installing crontab";
 # crontab crontab
 # crontab -l
 # echo "Finished installing crontab";
-
-# XXX: Consider auto-pulling this
 # SETUP_CRON_LINE="0 */6 * * * '$SETUP_FILE_PATH'"
 # command -v crontab 2>&1 > /dev/null \
 #     && (
@@ -106,19 +82,3 @@ fi
 # cd $DIR
 # EOF
 #     )
-
-# echo "Installing tools";
-# Install tools
-# Oh-my-zsh
-if [ ! -d $XDG_CONFIG_HOME/zsh/.oh-my-zsh ]; then
-    git clone git@github.com:ohmyzsh/ohmyzsh.git $XDG_CONFIG_HOME/zsh/.oh-my-zsh;
-fi
-# # Configure zsh as default shell
-if [ -n "`$SHELL -c 'echo $ZSH_VERSION'`" ]; then
-    # Assume in zsh
-    echo "";
-else
-    echo "Switching default shell to zsh, enter password:";
-    chsh -s $(which zsh);
-    echo "You may have to restart your terminal for changes to take effect";
-fi
