8b653a773737793e12ea8e69e0447eb21091d65a
michael-christen <mchristen96@gmail.com>
2025-06-20 08:32:12 -0700

    remap to comma

--- xdg_config_dir/nvim/lua/keymaps.lua
+++ xdg_config_dir/nvim/lua/keymaps.lua
@@ -7,8 +7,7 @@ local term_opts = { silent = true }
 local keymap = vim.api.nvim_set_keymap
 
 --Remap space as leader key
-keymap("", "<Space>", "<Nop>", opts)
--- XXX: Is this a problem / should it remain space
+keymap("", ",", "<Nop>", opts)
 vim.g.mapleader = ","
 vim.g.maplocalleader = ","
 
